4fdd68a6acdefd155d903aa0da471b4ffef28962
ZeroMarker <u78k4l67@163.com>
2023-05-21 17:09:38 +0000

    Add source code

---
+++ default.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+touch love_{1..10}_shiyanlou.txt
+
+# create 10 txt file
+
+who am i
+who mom like
+whoami
+
+sudo adduser lilei
+
+
+groups shiyanlou
+
+sudo usermod -G sudo lilei
+
+sudo deluser lilei --remove-home
+
+groupdel
+
+ls -asSh
+
+sudo chown shiyanlou iphone11
+
+chmod 600 iphone11
+chmod go-rw iphone11
+# group others - substract
+
+
+
